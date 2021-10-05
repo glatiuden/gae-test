2ca59e421e9cf6f1c081a4e6e720815eb6f5918d
Koh Vinleon <kohvinleon@gmail.com>
2021-10-05 09:09:22 +0800

    Add Travis

--- script.sh
+++ script.sh
@@ -1,3 +1,1 @@
-printf '%s\n' "$google_credentials" > "$TRAVIS_BUILD_DIR/client-secret.json"
-gcloud auth activate-service-account --key-file client-secret.json
 gcloud app deploy --project=cs3219-otot-task-b-gae app.yaml
